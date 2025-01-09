357b8605cfe57a829e93b7bf16a60e4d1819c7ab
vienpham2019 <vienpham2019@gmail.com>
2025-01-09 16:30:53 -0600

    add logo

--- src/layout/authLayout.jsx
+++ src/layout/authLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import authImage from "../img/Auth.png";
+import Logo from "../components/Logo";
 const AuthLayout = () => {
   return (
     <div className="font-[sans-serif] bg-gray-900 md:h-screen">
-      <div className="grid md:grid-cols-2 items-center gap-8 h-full">
+      <div className="grid md:grid-cols-2 items-center gap-8 h-full relative">
+        <Logo className={"absolute w-[10rem] -top-[2rem] m-0"} />
         <div className="max-md:order-1 p-4">
           <img
             src={authImage}
@@ -11,8 +13,7 @@ const AuthLayout = () => {
             alt="login-image"
           />
         </div>
-
-        <div className="flex items-center md:p-8 p-6 bg-white md:rounded-tl-[55px] md:rounded-bl-[55px] h-full">
+        <div className="max-md:pt-[5rem] flex items-center md:p-8 p-6 bg-white md:rounded-tl-[55px] md:rounded-bl-[55px] h-full">
           <Outlet />
         </div>
       </div>
